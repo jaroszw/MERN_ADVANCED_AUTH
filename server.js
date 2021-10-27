06a96c289da8b3faef9383288655241c63dbd336
Wojciech Jarosz <jaroszw@gmail.com>
2021-10-27 14:25:13 +0200

    MongoDB error handling

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use("/api/auth", router);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
 });
