48f8599febf86ffefa2dd96f26106012a2213682
Wojciech Jarosz <jaroszw@gmail.com>
2021-10-27 19:17:25 +0200

    Error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const User = require("../models/User.js");
+const User = require('../models/User.js');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.register = async (req, res, next) => {
-  res.send("Register Route");
+  console.log(req.body);
   const { username, email, password } = req.body;
   try {
     const user = await User.create({ username, email, password });
@@ -10,44 +11,47 @@ exports.register = async (req, res, next) => {
       user,
     });
   } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    next(error);
   }
 };
+
 
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || password) {
-    res
-      .status(400)
-      .json({ success: false, error: "Please provide email and password" });
+  // Check if email and password is provided
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
   }
 
   try {
-    const user = await User.findOne({ email }).select("+password");
+    // Check that user exists by email
+    const user = await User.findOne({ email }).select('+password');
 
     if (!user) {
-      res.status(404).json({ success: false, error: "Invalid credentials" });
+      return next(new ErrorResponse('Invalid email', 401));
     }
 
-    const isMatch = await user.matchPasswords();
+    // Check that password match
+    const isMatch = await user.matchPasswords(password);
+
     if (!isMatch) {
-      res.status(404).json({ success: false, error: "Invalid credentials" });
+      return next(new ErrorResponse('Invalid password', 401));
     }
 
     res.status(200).json({
-      success: true,
-      token: "1qashdur;naksr",
+      user,
+      token: '12kjsbndjlavs',
     });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 
 exports.forgotpassword = (req, res, next) => {
-  res.send("Forgot Password Route");
+  res.send('Forgot Password Route');
 };
 
 exports.resetpassword = (req, res, next) => {
-  res.send("Reset Password Route");
+  res.send('Reset Password Route');
 };
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-require("dotenv").config({ path: "./config.env" });
-const express = require("express");
-const router = require("./routes/auth");
+require('dotenv').config({ path: './.env' });
+const express = require('express');
+const router = require('./routes/auth');
 const app = express();
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 //Conect DB
 connectDB();
 
 app.use(express.json());
-app.use("/api/auth", router);
+app.use('/api/auth', router);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-process.on("unhandledRejection", (err, promise) => {
+process.on('unhandledRejection', (err, promise) => {
   console.log(`Logged Error: ${err}`);
   server.close(() => process.exit(1));
 });
