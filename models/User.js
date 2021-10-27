dd5ba90bfbb108d5fe290d44b58e23cb6aeb5677
Wojciech Jarosz <jaroszw@gmail.com>
2021-10-27 13:15:52 +0200

    MERN AUTH Borderplate

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: [true, "Please provide a username"] },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+  },
+});
+
+const User = mopnguse.model("User", userSchema);
