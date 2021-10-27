2f9b87b55bb93f434c15e4a262f185619a586708
Wojciech Jarosz <jaroszw@gmail.com>
2021-10-27 15:13:23 +0200

    login and register controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,8 +14,34 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send("Login Route");
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || password) {
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+    }
+
+    const isMatch = await user.matchPasswords();
+    if (!isMatch) {
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: "1qashdur;naksr",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 exports.forgotpassword = (req, res, next) => {
